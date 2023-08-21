bc303782219725757e03572b245865caf540b2b7
Paweł Jarecki <paweljarecki10@gmail.com>
2023-08-21 11:24:12 +0200

    Connected front and back-end + modified files

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {UserEntity} from 'types'
 
 function App() {
+
+  const foobar: UserEntity = {
+    name: '123'
+  }
   return (
     <div className="App">
       <header className="App-header">
