ebaf8499394a3d68c382b90ea880d92041522012
Paweł Jarecki <paweljarecki10@gmail.com>
2023-08-25 20:50:34 +0200

    Added possibility to register account and log in to it

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,23 @@ import './App.css';
 import { SearchContext } from './components/search/SearchContext';
 import { SearchComponent } from './components/search/SearchComponent';
 import {AllTradesList} from "./components/trades/AllTradesList";
+import {RegForm} from "./components/logreg/RegForm";
+import {LogForm} from "./components/logreg/LogForm";
 
 export const App = () => {
 
     const [search, setSearch] = useState('')
 
+    const refreshMath = () => {
+        window.location.replace("http://localhost:3000");
+    }
+
     return (
         <div>
             <SearchContext.Provider value={{search, setSearch}}>
-                <AllTradesList/>
+                {/*<AllTradesList/>*/}
+                {/*<RegForm/>*/}
+                <LogForm refresh={refreshMath}/>
             </SearchContext.Provider>
         </div>
     )
