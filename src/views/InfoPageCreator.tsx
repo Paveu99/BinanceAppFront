9e82eb085a18a638d74ae782a6fa2d52e7a5ff8d
Paweł Jarecki <paweljarecki10@gmail.com>
2023-09-13 19:50:37 +0200

    New view created to show information about the creator

---
+++ src/views/InfoPageCreator.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const InfoPageCreator = () => {
+  return <div style={{textAlign: "center", marginTop: "100px"}}>
+    <h2>Information about the creator</h2>
+    <p>author: Paweł Jarecki</p>
+    <p>email: <a href="mailto:paweljarecki10@gmail.com?subject=Question&body=Description">paweljarecki10@gmail.com</a></p>
+    <p>GitHub repository: <a href="https://github.com/Paveu99" target="_blank">github.com/Paveu99</a></p>
+  </div>
+}
