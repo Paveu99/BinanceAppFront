71ce4a49a2e31c39b3c95f4b8a81e61ba5f2d0eb
Paweł Jarecki <paweljarecki10@gmail.com>
2023-09-17 21:48:18 +0200

    Added new style to the favourite rade

--- src/components/trades/SingleFavTrade.tsx
+++ src/components/trades/SingleFavTrade.tsx
@@ -3,7 +3,6 @@ import {TradeEntity} from "types";
 import {Modal} from "../modal/Modal";
 import up from "../styles/Green_Arrow_Up_Darker.svg.png";
 import down from "../styles/900px-Red_Arrow_Down.svg.png";
-import '../styles/SingleTrade.css'
 
 interface Props {
     trade: TradeEntity
@@ -32,11 +31,80 @@ export const SingleFavTrade = (props: Props) => {
     }
 
     const response = <div className="singleFavTrade">
-        {props.trade.symbol} and {props.trade.weightedAvgPrice}
-        <button onClick={deleteTrade}>Delete</button>
-        <button onClick={() => setOpenModal(!openModal)}>Show modal</button>
+        <div className="overalInfo">
+            <div className="tradeName">
+                {props.trade.symbol}
+            </div>
+        </div>
+        <div className="info">
+            <div className="main">
+                <div className="spec">
+                    <div className="desc">
+                        Price Change Percent
+                    </div>
+                    <div className="value">
+                        {
+                            Number(props.trade.priceChangePercent) > 0
+                                ? <div>{props.trade.priceChangePercent}%<img src={up} style={{width: '30px', paddingLeft: "10px"}}/></div>
+                                : <div>{props.trade.priceChangePercent}%<img src={down} style={{width: '30px', paddingLeft: "10px"}}/></div>
+                        }
+                    </div>
+                </div>
+                <hr/>
+                <div className="spec">
+                    <div className="desc">
+                        Weighted Average Price
+                    </div>
+                    <div className="value">
+                        {props.trade.weightedAvgPrice}
+                    </div>
+                </div>
+            </div>
+            <div className="detailed">
+                <div className="upper">
+                    <div className="spec">
+                        <div className="desc">
+                            Open Price
+                        </div>
+                        <div className="value">
+                            {props.trade.openPrice}
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="spec">
+                        <div className="desc">
+                            Last Price
+                        </div>
+                        <div className="value">
+                            {props.trade.lastPrice}
+                        </div>
+                    </div>
+                </div>
+                <hr/>
+                <div className="lower">
+                    <div className="spec">
+                        <div className="desc">
+                            High Price
+                        </div>
+                        <div className="value">
+                            {props.trade.highPrice}
+                        </div>
+                    </div>
+                    <hr/>
+                    <div className="spec">
+                        <div className="desc">
+                            Low Price
+                        </div>
+                        <div className="value">
+                            {props.trade.lowPrice}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button className="button" onClick={deleteTrade}>Delete</button>
+        <button  className="button" onClick={() => setOpenModal(!openModal)}>Show modal</button>
         <Modal info={props.trade} isOpen={openModal} onClose={() => setOpenModal(false)}/>
-        {Number(props.trade.priceChangePercent) > 0 ? <img src={up} style={{width: '30px'}}/> : <img src={down} style={{width: '30px'}}/>}
     </div>
 
     return (response)
