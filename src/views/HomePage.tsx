16f8ad7e0568c765d4aa6fb09d06aa8d697f6ccd
Paweł Jarecki <paweljarecki10@gmail.com>
2023-09-19 12:41:16 +0200

    Home Page is now done

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,5 +1,14 @@
 import React from "react";
-
+import '../components/styles/HomePage.css'
 export const HomePage = () => {
-    return <p>Home page</p>
+    return <div className="container">
+        <div className="row">
+            <div className="col-md-12 text-center">
+                <h3 className="animate-charcter">BINANCE APP</h3>
+            </div>
+        </div>
+        <div style={{textAlign: "center", fontSize: "25px"}}>
+            WEB APPLICATION THAT HELPS YOU FIND INFORMATION ABOUT BINANCE TRADES
+        </div>
+    </div>
 }
