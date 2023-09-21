a5c85e18d681341a2b4783002f7ddc89be8aefb0
Paweł Jarecki <paweljarecki10@gmail.com>
2023-09-21 14:52:27 +0200

    Changes in the "a" component

--- src/views/InfoPage.tsx
+++ src/views/InfoPage.tsx
@@ -5,6 +5,8 @@ export const InfoPage = () => {
     <h1>Information about the application:</h1>
     <p>It is a web application, that uses Binance API to help you gather data about cryptocurrencies. Web app gives you opportunity to check values of chosen cryptocurrencies and how they convert to different cryptocurrencies.</p>
     <p>By using build in calculator and opportunity to add your favourites, you can log in to check how the market is looking.</p>
-    <p>Information comes from: <a href="https://danepubliczne.imgw.pl" target="_blank">https://api2.binance.com/api/v3/ticker/24hr</a>.</p>
+    <p>Information comes from: <a href="https://api2.binance.com/api/v3/ticker/24hr" target="_blank">https://api2.binance.com/api/v3/ticker/24hr</a>.</p>
+    <hr/>
+    <h2>Step by step tutorial:</h2>
   </div>
 }
