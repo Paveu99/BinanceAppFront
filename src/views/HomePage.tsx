9abdfcd0f5e50e28eaac1e0b5c04b4cf10c7da2e
Paweł Jarecki <paweljarecki10@gmail.com>
2023-12-09 18:17:01 +0100

    Typo

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -4,7 +4,11 @@ export const HomePage = () => {
     return <div className="container">
         <div className="row">
             <div className="col-md-12 text-center">
-                <h3 className="animate-charcter"><a href="https://www.binance.com/en" rel="noreferrer" target="_blank">BINANCE APP</a></h3>
+                <h3 className="animate-charcter">
+                    <a href="https://www.binance.com/en" rel="noreferrer" target="_blank">
+                        BINANCE APP
+                    </a>
+                </h3>
             </div>
         </div>
         <div style={{textAlign: "center", fontSize: "25px"}}>
